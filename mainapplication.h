287671ab1f2da0af87fd187240263a6d1766786a
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-08 11:48:25 +0300

    new: add MainApplication Class

---
+++ mainapplication.h
@@ -0,0 +1,12 @@
+#ifndef MAINAPPLICATION_H
+#define MAINAPPLICATION_H
+
+#include <QtCore/qglobal.h>
+
+class MainApplication : public WApplication
+{
+public:
+    MainApplication();
+};
+
+#endif // MAINAPPLICATION_H
