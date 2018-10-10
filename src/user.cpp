b8c51ef8e98c9b6550f880e56d351ff4450f8956
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-10-10 14:34:06 +0300

    User Class Declaration/Defination Completed.

--- src/user.cpp
+++ src/user.cpp
@@ -4,3 +4,100 @@ User::User()
 {
 
 }
+
+std::string User::getAdsoyad() const
+{
+    return adsoyad;
+}
+
+void User::setAdsoyad(const std::string &value)
+{
+    adsoyad = value;
+}
+
+std::string User::getCeptel() const
+{
+    return ceptel;
+}
+
+void User::setCeptel(const std::string &value)
+{
+    ceptel = value;
+}
+
+
+std::string User::getAddres() const
+{
+    return addres;
+}
+
+void User::setAddres(const std::string &value)
+{
+    addres = value;
+}
+
+std::string User::getTcno() const
+{
+    return tcno;
+}
+
+void User::setTcno(const std::string &value)
+{
+    tcno = value;
+}
+
+std::string User::getPassword() const
+{
+    return password;
+}
+
+void User::setPassword(const std::string &value)
+{
+    password = value;
+}
+
+bool User::isValid() const
+{
+    bool valid = true;
+
+    if( tcno.length() == 0 )
+    {
+        valid = false;
+    }
+
+    if( adsoyad.length() == 0 )
+    {
+        valid = false;
+    }
+
+    if( ceptel.length() != 11 )
+    {
+        valid = false;
+    }
+
+    if( addres.length() == 0 )
+    {
+        valid = false;
+    }
+
+    if( password.length() == 0 )
+    {
+        valid = false;
+    }
+
+    if( universite.length() == 0 )
+    {
+        valid = false;
+    }
+    return valid;
+}
+
+std::string User::getUniversite() const
+{
+    return universite;
+}
+
+void User::setUniversite(const std::string &value)
+{
+    universite = value;
+}
--- src/user.h
+++ src/user.h
@@ -1,12 +1,45 @@
 #ifndef USER_H
 #define USER_H
 
-#include <QtCore/qglobal.h>
+#include <string.h>
+#include <iostream>
 
+using namespace std;
 class User
 {
 public:
     User();
+
+    std::string getAdsoyad() const;
+    void setAdsoyad(const std::string &value);
+
+    std::string getCeptel() const;
+    void setCeptel(const std::string &value);
+
+
+    std::string getAddres() const;
+    void setAddres(const std::string &value);
+
+    std::string getTcno() const;
+    void setTcno(const std::string &value);
+
+    std::string getPassword() const;
+    void setPassword(const std::string &value);
+
+    bool isValid() const;
+
+    std::string getUniversite() const;
+    void setUniversite(const std::string &value);
+
+private:
+    std::string adsoyad;
+    std::string ceptel;
+    std::string universite;
+    std::string addres;
+    std::string tcno;
+    std::string password;
 };
+
+
 
 #endif // USER_H
